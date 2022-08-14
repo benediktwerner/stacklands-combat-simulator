b9b6435e354e363591e5b6570ff54795bd44a58e
Benedikt Werner <1benediktwerner@gmail.com>
2022-08-14 15:00:05 +0200

    Fix deploy script on second run

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 set -e
 cd svelte/dist
 git init
-git checkout -b master
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:benediktwerner/stacklands-combat-simulator.git master:gh-pages
